f0d492b689ed571083ce9fe5a75f6c933637ecad
arumanuevo <glipischeck@gmail.com>
2026-07-09 19:20:20 -0300

    fsdfsdf

--- app/Http/Controllers/ImportMedicionesController.php
+++ app/Http/Controllers/ImportMedicionesController.php
@@ -41,8 +41,8 @@ class ImportMedicionesController extends Controller
      */
     public function showImportForm()
     {
-        return "El método showImportForm() está funcionando";
-        //return view('import-mediciones');
+        //return "El método showImportForm() está funcionando";
+        return view('import-mediciones');
     }
 
     /**
