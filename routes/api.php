8a85a11979950a0e1d9d26846d9ff22f905836a7
arumanuevo <glipischeck@gmail.com>
2026-07-13 10:49:12 -0300

    sssss

--- routes/api.php
+++ routes/api.php
@@ -44,7 +44,7 @@ Route::post('/import-mediciones/import', [App\Http\Controllers\ImportMedicionesC
 
 Route::middleware('auth:sanctum')->group(function () {
     // Rutas existentes
-    Route::get('/getMedidor', [App\Http\Controllers\ApiGeneral::class, 'getMedidor'])->name('getMedidor');
+    
     Route::get('/getToken', [App\Http\Controllers\ApiGeneral::class, 'getToken'])->name('getToken');
     Route::post('/postMed', [App\Http\Controllers\ApiGeneral::class, 'postMed'])->name('postMed');
     Route::post('/postBorrarMedicion', [App\Http\Controllers\ApiGeneral::class, 'postBorrarMedicion'])->name('postBorrarMedicion');
@@ -55,3 +55,5 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('/getGuardarFacturas', [App\Http\Controllers\ApiGeneral::class, 'getGuardarFacturas'])->name('getGuardarFacturas');
     Route::post('/postGuardarFacturas', [App\Http\Controllers\ApiGeneral::class, 'postGuardarFacturas'])->name('postGuardarFacturas');
 });
+
+Route::get('/getMedidor', [App\Http\Controllers\ApiGeneral::class, 'getMedidor'])->name('getMedidor');
